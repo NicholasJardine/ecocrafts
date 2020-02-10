e2efaf93c53a61958bb303dd87a87567c155db20
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-02-10 10:21:58 +0200

    okay

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,14 @@ indoors = Category.new(name: 'Home interior', photo_url: 'Indoors.jpg')
 
 puts "creating seeds"
 
-bedside = Product.create(name: 'Art', image_url: , sub_category_name: 'bed')
+bedside = Product.create(name: 'Bedside Table', image: 'bedside.jpeg', sub_category_name: 'bed')
+rocking_horse = Product.create(name: 'Rockinghorse', image: 'rockinghorse.jpeg', sub_category_name: 'toys')
+mirror_one = Product.create(name: 'bathroom mirror', image: 'brmirror.jpeg', sub_category_name: 'bath')
+planter_one = Product.create(name: 'Large outdoor planter', image: 'planter.jpeg', sub_category_name: 'vege-patch')
+bedside = Product.create(name: 'Art', image: 'bedside.jpeg', sub_category_name: 'bed')
+bedside = Product.create(name: 'Art', image: 'bedside.jpeg', sub_category_name: 'bed')
+bedside = Product.create(name: 'Art', image: 'bedside.jpeg', sub_category_name: 'bed')
+bedside = Product.create(name: 'Art', image: 'bedside.jpeg', sub_category_name: 'bed')
 
     t.string "name"
     t.string "image_url"
