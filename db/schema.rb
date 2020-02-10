311dc3068ceb37cdfa45a99b91b1d9648c449d7f
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-02-10 09:18:29 +0200

    started the index pages of the various products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_213829) do
+ActiveRecord::Schema.define(version: 2020_02_09_214300) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_02_09_213829) do
     t.string "sub_category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
